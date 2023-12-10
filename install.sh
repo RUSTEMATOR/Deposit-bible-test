b544ee4cb49aa0ab42c6c0b2c005700666d46e41
RUSTEMATOR <139997441+RUSTEMATOR@users.noreply.github.com>
2023-12-10 20:50:08 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install Playwright dependencies
+npm install playwright
+
+# Install any other dependencies your project may need
+# For example, if there are Python dependencies:
+pip install -r requirements.txt
